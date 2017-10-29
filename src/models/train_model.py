4f85ae9e4e11426b670e69305ebfb0501369fa89
djb15 <dirk.judd@live.com>
2017-10-29 12:39:25 +0000

    feat: Group original and blurred images together for training

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,21 +1,39 @@
 import tensorflow as tf
 import numpy as np
 import os
+import re
 
 def read_image(filename_queue):
     reader = tf.WholeFileReader()
+
     key, value = reader.read(filename_queue)
-    image = tf.image.decode_jpeg(value, channels=3)
-    return key, image
+    original = tf.image.decode_jpeg(value, channels=3)
+
+    key, value = reader.read(filename_queue)
+    blurred = tf.image.decode_jpeg(value, channels=3)
+
+    return original, blurred
 
 
 def input_data():
     project_dir = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir)
     raw_data_path = os.path.join(project_dir, "data", "raw", "pre-blur")
-    raw_data_filepaths = [os.path.join(raw_data_path, f) for f in os.listdir(raw_data_path)][:2]
-    raw_data_queue = tf.train.string_input_producer(raw_data_filepaths)
-    filename, image = read_image(raw_data_queue)
-    return filename, image
+    raw_data_filenames = os.listdir(raw_data_path)
+
+    blurred_data_path = os.path.join(project_dir, "data", "processed")
+    blurred_data_filenames = os.listdir(blurred_data_path)
+
+    grouped_data = []
+
+    for image_name in blurred_data_filenames:
+        corresponding_raw = image_name.split('-blurred-')[0] + '.jpg'
+        grouped_data.append(os.path.join(raw_data_path, corresponding_raw)) # Append original first then blurred
+        grouped_data.append(os.path.join(blurred_data_path, image_name))
+
+
+    raw_data_queue = tf.train.string_input_producer(grouped_data)
+    original, blurred = read_image(raw_data_queue)
+    return original, blurred
 
 
 def build_network(input_layer):
@@ -141,7 +159,7 @@ def build_network(input_layer):
 
 
 def main(argv=None):
-    label, image = input_data()
+    original, blurred = input_data()
     #image_batch = tf.train.batch([image], batch_size=30)
     #label_batch = tf.train.batch([label], batch_size=30)
 
