9defaf241b62e179b65a0661c5d669e5acd1a8e6
Dirk Brink <dirk.judd@live.com>
2017-11-10 22:42:54 +0000

    edit: Checkpoint files and model loading attempt that doesn't run

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -2,7 +2,14 @@ import tensorflow as tf
 import os
 
 sess = tf.Session()
-saver = tf.train.import_meta_graph('trained-1000.meta')
+saver = tf.train.import_meta_graph('../features-115000.meta')
 saver.restore(sess,tf.train.latest_checkpoint('../'))
 
-sess.run("predict:0")
+epochs = 100
+
+for step in range(epochs):
+    _, loss_val = sess.run("Total_loss:0")
+    duration = time.time() - start_time
+    print(
+        "Epoch {step}/{total_steps}\nBatch Loss: {:.4f}\nTime:{:.2f}s\n---"
+        .format(loss_val, duration, step=step, total_steps=epochs - 1))
